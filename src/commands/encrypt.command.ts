a9551299fde3263738f252f83e6956393fc61fc0
marc.marcet@codinghumans.com <marc.marcet@codinghumans.com>
2020-10-26 11:17:18 +0100

    chore: Remove unused imports

--- src/commands/encrypt.command.ts
+++ src/commands/encrypt.command.ts
@@ -1,13 +1,8 @@
 import { Configfile, configfile } from '../lib/configfile';
-import { decrypt, encrypt, encryptAll } from '../lib/utils/file.utils';
 
 import { Command } from './command';
 import { ConfigurationError } from '../errors/configuration.error';
-import { Keychain } from '../lib/keychain';
-import { MasterKey } from '../lib/master-key';
-import fs from 'fs-extra';
-import glob from 'glob';
-import globby from 'globby';
+import { encryptAll } from '../lib/utils/file.utils';
 
 export class EncryptCommand implements Command {
 	async execute(): Promise<void> {
